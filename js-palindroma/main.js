d23d1743fc96ba22302357f5031447fc9741f1e2
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-02 13:01:31 +0100

    feat: complete exercise one

--- js-palindroma/main.js
+++ js-palindroma/main.js
@@ -2,3 +2,22 @@
 
 const yourWord = prompt('Inserisci una parola');
 console.log(yourWord);
+
+// creare una funzione per capire se la parola inserita è palindroma
+
+function isPalindromeWord (word) {
+    
+    let reversedWord = '';
+    
+    for (let i = word.length -1; i >=0; i--) {
+
+        reversedWord = reversedWord + word[i];
+    }
+
+    // True o false
+    
+    return reversedWord === yourWord;
+
+    }
+    
+console.log(isPalindromeWord(yourWord));
