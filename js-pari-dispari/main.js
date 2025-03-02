50976d255bc9ed25dcf395622ac15826f2711198
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-02 22:01:38 +0100

    feat: add second prompt

--- js-pari-dispari/main.js
+++ js-pari-dispari/main.js
@@ -1,6 +1,11 @@
 
-// 'pari o dispari?' 
+// utente inserisce la scelta 'pari' o 'dispari'
 
 const chooseEvenOrOdd = prompt("Scegli pari o dispari");
 console.log(chooseEvenOrOdd);
+
+// utente inserisce un numero da 1 a 5 in base alla scelta fatta in precedenza
+
+const uNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log(uNumber);
 
